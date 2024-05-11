2304ed4f73609c3bbb2c81295273b8f796456a81
VarvaraTrybiuk <ipz221_tvo@student.ztu.edu.ua>
2024-05-11 12:18:01 +0300

    Added Game window

---
+++ MySudokuGame/MySudoku/Game.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Hints;
+using Solver;
+using SudokuComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MySudoku
+{
+    /// <summary>
+    /// Interaction logic for Gamexaml.xaml
+    /// </summary>
+    public partial class Gamexaml : Window
+    {
+        private SudokuGame game;
+
+        public Gamexaml()
+        {
+            InitializeComponent();
+            game = new SudokuGame(SudokuLvl.Normal);
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            game.StartGame();
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    TextBox textBox = new TextBox();
+                    if (game.GetCells()[i] != null && game.GetCells()[i].row == i && game.GetCells()[i].col == j)
+                    {
+                        textBox.Text = game.GetCells()[i].value.ToString();
+                        textBox.IsReadOnly = true;
+                    }
+                    else
+                    {
+                        textBox.TextChanged += Cell_TextChanged;
+                       
+                    }
+                    Field.Children.Add(textBox);
+                    Grid.SetRow(textBox, i);
+                    Grid.SetColumn(textBox, j);
+                }
+            }
+            
+        }
+
+        private void Cell_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox? textBox = sender as TextBox;
+            if (textBox != null)
+            {
+                int row = Grid.GetRow(textBox);
+                int column = Grid.GetColumn(textBox);
+                if (int.TryParse(textBox.Text, out int value))
+                {
+                    game.AddToBoard([row, column], value);
+                    textBox.IsReadOnly = true;
+                }
+            }
+        }
+      
+        private void Random(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Specific(object sender, RoutedEventArgs e)
+        {
+        }
+      
+
+        private void CheckSudoku(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UndoStep(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+      
+    }
+}
---
+++ MySudokuGame/SudokuComponents/SudokuGame.cs
@@ -0,0 +1,77 @@
+﻿using Hints;
+using Solver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuComponents
+{
+    public class SudokuGame
+    {
+        private Board board;
+        private IHint? _hint;
+        private Generator _solver;
+        public SudokuGame(SudokuLvl lvl)
+        {
+            this.board = new Board((int)lvl);
+            _solver = new Generator(lvl);
+        }
+        public void StartGame()
+        {
+            _solver.Generate();
+            GenerateBoard();
+        }
+
+        private void GenerateBoard()
+        {
+            int size = _solver.SolvedSudoku.GetLength(0);
+            var random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                int j = random.Next(1, size);
+                board.TryFillCell(new Cell([i, j], _solver.SolvedSudoku[i, j]));
+            }
+        }
+        public void AddToBoard(int[] values, int n)
+        {
+            board.Draft.Add(new Cell(values, n));
+        }
+        public bool OpenCell(IHint hint)
+        {
+            var result = hint.Execute();
+            if (result != null)
+            {
+                if (!board.TryFillCell(new Cell(result, _solver.SolvedSudoku[result[0], result[1]])))
+                {
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public bool Check(int[] values, int n)
+        {
+            int i = values[0];
+            int j = values[1];
+            if (_solver.SolvedSudoku[i, j] == n)
+            {
+                board.TryFillCell(new Cell(values, n));
+                return true;
+            }
+            return false;
+        }
+        public List<Cell> GetCells()
+        {
+            return board.cells;
+        }
+        public List<Cell> GetDraft()
+        {
+            return board.Draft;
+        }
+    }
+
+}
